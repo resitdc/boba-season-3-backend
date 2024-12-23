2a80e61763ae2cb69199da9a59bdb3c2a57a0097
Restu Dwi Cahyo <resitdc@resitdc-2.local>
2024-12-23 19:59:53 +0700

    Finish CRUD

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 const express = require('express')
 const mysql = require("mysql")
-const app = express()
+const cors = require("cors")
+global.app = express()
 
-const connection = mysql.createConnection({
-  host      : '194.233.70.154',
-  user      : 'boba',
-  password  : '12345678',
-  database  : 'belajar'
+global.connection = mysql.createConnection({
+  host      : 'localhost',
+  user      : 'root',
+  password  : '',
+  database  : 'bacod_cofeshop'
 });
 
+app.use(cors());
+app.use(express.json());
 connection.connect();
 
-app.get('/siswa', function (req, res) {
-  connection.query('SELECT nis, nama FROM siswa', function (error, results, fields) {
-    res.json(results)
-  });
-});
-
-app.get('/agama', function (req, res) {
-  connection.query('SELECT * FROM agama', function (error, results, fields) {
-    res.json(results)
-  });
-});
+require("./module/menu")
 
 app.listen(3000)
