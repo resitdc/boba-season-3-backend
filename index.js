34b937851bc707ba4976613d255b405472f11f03
Restu Dwi Cahyo <resitdc@resitdc-2.local>
2024-12-23 20:17:51 +0700

    New version

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express')
-const mysql = require("mysql")
 const cors = require("cors")
 global.app = express()
 
-global.connection = mysql.createConnection({
-  host      : 'localhost',
-  user      : 'root',
-  password  : '',
-  database  : 'bacod_cofeshop'
+global.knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'bacod_cofeshop',
+  },
 });
 
 app.use(cors());
 app.use(express.json());
-connection.connect();
 
 require("./module/menu")
 
--- module/menu.js
+++ module/menu.js
@@ -1,30 +1,36 @@
-app.get('/menu', function (req, res) {
-  connection.query('SELECT * FROM menu', function (error, results, fields) {
-    res.json(results)
-  });
+app.get('/menu', async (req, res) => {
+  const results = await knex('menu').select();
+  res.json(results);
 });
 
-app.post("/menu", (req, res) => {
+app.post("/menu", async (req, res) => {
   const data = req.body
 
-  connection.query(`INSERT INTO menu (name, description, price, stock) VALUES ('${data.name}', '${data.description}', '${data.price}', '${data.stock}')`, (error, results, fields) => {
-    res.json(results)
-  });
+  const results = await knex('menu').insert({
+    name: data.name,
+    description: data.description,
+    price: data.price,
+    stock: data.stock,
+  })
+
+  res.json(results)
 });
 
-app.delete("/menu/:id", (req, res) => {
+app.delete("/menu/:id", async (req, res) => {
   const primaryKey = req.params.id
   
-  connection.query(`DELETE FROM menu WHERE id = '${primaryKey}'`, (error, results, fields) => {
-    res.json(results)
-  });
+  const results = await knex("menu").where("id", primaryKey).del();
+  res.json(results)
 });
 
-app.put("/menu/:id", (req, res) => {
+app.put("/menu/:id", async (req, res) => {
   const primaryKey = req.params.id
   const data = req.body
   
-  connection.query(`UPDATE menu SET name = '${data.name}', price = '${data.price}', stock = '${data.stock}' where id = '${primaryKey}'`, (error, results, fields) => {
-    res.json(results);
+  const results = await knex("menu").where("id", primaryKey).update({
+    name: data.name,
+    price: data.price,
+    stock: data.stock,
   });
+  res.json(results)
 });
